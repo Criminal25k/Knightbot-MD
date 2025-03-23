8792920cf08dd6d6234b2e3e22b3a3103c04bf7f
Criminal25k <albinjaison50@gmail.com>
2025-03-24 01:50:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Criminal Bot",
+  botOwner: 'Ark criminal', // Your name
+  ownerNumber: '916282945816', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
